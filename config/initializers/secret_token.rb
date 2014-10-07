edb6886d2c66dd7a9ec0df54af671c4d219e404b
Tony Plaza <tplaza3@gmail.com>
2014-10-07 12:18:05 -0500

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NonprofitamInfrastructureChecklist::Application.config.secret_token = '481ca4fa9ffc3c09367f6078b1354a7b80c4251524e7b902ead4458e55d6f2ad54939b8fccca653944cadc30f39de0e0ef2a23f2e63cea9ce19fa1c0438ac6cc'
