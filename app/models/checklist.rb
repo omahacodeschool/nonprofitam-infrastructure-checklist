907872e1ddcb893fbb666ffe617a2449193ffeff
Nathaniel Ruiz <Nathruiz13@gmail.com>
2014-10-10 15:33:33 -0500

    Refactored and Moved Response methods

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -3,5 +3,22 @@ class Checklist < ActiveRecord::Base
   
   
   has_many :questions 
-  accepts_nested_attributes_for :questions, allow_destroy: true
+  accepts_nested_attributes_for :questions, allow_destroy: true\
+  
+  
+  # Public: Counts the integer total amount of questions associated to a given Checklist(n)
+  #
+  # @hecklist  - A given ActiveRecord Checklist
+  # @total - Uses ActiveRecord associations to find number of questions
+  #
+  # Examples
+  #
+  #   total_questions(3)
+  #   # => 10
+  #
+  def total_questions
+      total =  self.questions.length
+      total 
+    end
+  
 end
