5414b75dae4c54765ec212f6f2ad555ec65851d9
Nathaniel Ruiz <Nathruiz13@gmail.com>
2014-10-10 15:06:34 -0500

    Sweet, Sweet percentage calculation methods

--- app/models/response.rb
+++ app/models/response.rb
@@ -5,6 +5,12 @@ class Response < ActiveRecord::Base
   belongs_to :user
   
   
+  def total_questions(n)
+      @checklist = Checklist.find(n)
+      @total =  @checklist.questions.length
+      @total 
+    end
+  
   def counts(n) 
     count = Hash.new(0)
     a_hash = self.answers_hash
@@ -12,6 +18,17 @@ class Response < ActiveRecord::Base
     answers.each{ |ans| count[ans] += 1}
     count[n]
   end
+  
+  def percent(value, checklist_id)
+    ans = []
+    
+    Checklist.find(checklist_id).questions.each do |q|
+      ans << self.answers_hash["#{q.id}"] 
+    end
+    count = Hash.new(0)
+    ans.each{ |e| count[e] += 1 }
+    (((count[value].to_f/total_questions(checklist_id).to_f)*100).to_i).to_s + "%"
+   end
   
     
 end
