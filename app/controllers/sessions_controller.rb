ea056abfb76dcd10d715c156bd79ff6e0d966070
Nathaniel Ruiz <Nathruiz13@gmail.com>
2014-10-11 11:48:14 -0500

    CSS edits

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = login(params[:email], params[:password])
     if user
-      redirect_back_or_to root_url, :notice => "Logged in!"
+      redirect_back_or_to checklists_path, :notice => "Logged in!"
     else
       flash.now.alert = "Email or password was invalid"
       render :new
